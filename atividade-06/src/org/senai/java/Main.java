9e1e00b20ac2150b31d38362bd111f121e5d59d0
Aclecio Cruz <aclecioscruz@gmail.com>
2021-06-22 14:52:24 -0300

    Resolução da atividade 06

--- atividade-06/src/org/senai/java/Main.java
+++ atividade-06/src/org/senai/java/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Pessoa pessoa = new Pessoa("Aclécio", 8, 4, 1979);
+		
+		System.out.println(pessoa.getNome()); 
+		System.out.println(pessoa.getDataNascimento()); 
+		System.out.println(pessoa.calculaIdade()); 
+		
+		pessoa.setNome("Aclecio Cruz");
+		System.out.println(pessoa.getNome());
 	}
 
 }
